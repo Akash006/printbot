3bfdc85bcc77c8023b0a63a87f784e950c224f63
Akash006 <mastermindvishu97@gmail.com>
2020-11-12 12:01:08 +0530

    Update and rename AdvancePrint to AdvancePrint.py
    Adding custom config options

--- AdvancePrint.py
+++ AdvancePrint.py
@@ -8,8 +8,31 @@ from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
                           ConversationHandler)
 import os, time
 import datetime
+import logging
 
-COPIES, SIZE, DUPLEX, TRAY, COLOR = range(5)   
+COLOR, MORE, COPIES, SIZE, DUPLEX, TRAY = range(6)
+
+default = {
+    "color" : 0,
+    "copies" : 1,
+    "size" : "A4",
+    "duplex" : 1,
+    "tray" : 1,
+    "doc_path" : "/home/pi/scripts/PrintBot/doc",
+    "photo_path" : "/home/pi/scripts/PrintBot/photo",
+    "default_file" : "/home/pi/scripts/PrintBot/config/default.cfg",
+    "log_file" : "/home/pi/scripts/PrintBot/config/printBot.log"
+}
+
+def createConfig():
+    with open(default["default_file"], 'w') as df:
+        df.write("copies = " + str(default["copies"]) + "\n")
+        df.write("color = " + str(default["color"]) + "\n")
+        df.write("size = " + str(default["size"]) + "\n")
+        df.write("duplex = " + str(default["duplex"]) + "\n")
+        df.write("tray = " + str(default["tray"]) + "\n")
+
+    log.info("Configuration file updated successfully")
 
 def send_typing_action(func):
     """Sends typing action while processing func command."""
@@ -21,74 +44,104 @@ def send_typing_action(func):
 
     return command_func
 
-def echo(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Hey !!!\nDidn't get you please try again.")
-
 @send_typing_action
 def photo(update, context):
     """ Processing image """
     dt = datetime.datetime.now()
     updates = context.bot.get_updates()
-    update.message.reply_text("Nice Image !!\nEnter the No. of copies : ")
+    reply_keyboard = [["B/W","Color"],["/cancel"]]
+    update.message.reply_text("Nice Image !!\nYou want B/W or colored print ?", reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
     newFile = update.message.photo[-1].get_file()
     newFile.download('doc/{0}_{1}'.format(dt.strftime("%d%b%y_%H%M%S"), os.path.basename(newFile["file_path"])))
+    log.info("Photo received {0}".format(os.path.basename(newFile["file_path"])))
     
-    return COPIES
+    return COLOR
 
 @send_typing_action
 def doc(update, context):
     """ Processing documents """
     dt = datetime.datetime.now()
+    reply_keyboard = [["B/W","Color"],["/cancel"]]
     updates = context.bot.get_updates()
-    update.message.reply_text("Processing your document !!!\nEnter the No. of copies : ")
+    update.message.reply_text("Processing your document !!!\nYou want B/W or colored print ?", reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
     newFile = update.message.effective_attachment.get_file()
     newFile.download('doc/{0}_{1}'.format(dt.strftime("%d%b%y_%H%M%S"), os.path.basename(newFile["file_path"])))
+    log.info("Document received {0}".format(os.path.basename(newFile["file_path"])))
     
-    return COPIES
+    return COLOR
     
 @send_typing_action
 def copies(update, context):
     copy = update.message.text
-    reply_keyboard = [["A4", "A3", "/cancel"]]
+    default["copies"] = int(copy)
+    reply_keyboard = [["A4", "A3"], ["/cancel"]]
     update.message.reply_text("OK. You want {0} copies.\nPlease choose the paper size ?".format(copy),reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+    log.info(f"No of copies : {copy}")
     return SIZE
     
 @send_typing_action
 def size(update, context):
     size = update.message.text
-    reply_keyboard = [["Single", "Double", "/cancel"]]
+    default["size"] = size
+    reply_keyboard = [["Single", "Double"], ["/cancel"]]
     update.message.reply_text("Paper size : {0}\nYou want single side or double side ?".format(size),reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+    log.info(f"paper size choose {size}")
     return DUPLEX
     
 @send_typing_action
 def duplex(update, context):
-    reply_keyboard = [["1","2","3","/cancel"]]
+    reply_keyboard = [["1","2","3"], ["/cancel"]]
     duples = update.message.text
+
+    default["duplex"] = 0 if duples == "Single" else 1
+
     update.message.reply_text("OK. You want {0} side\nChoose the paper tray.".format(duples),reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+    log.info(f"Duplex choosen is {duples}")
     return TRAY
     
 @send_typing_action
 def tray(update, context):
     tray = update.message.text
-    reply_keyboard = [["B/W","Color","/cancel"]]
-    update.message.reply_text("Paper will be choosen from tray {0}.\nYou want B/W or colored print ?".format(tray),reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
-    return COLOR
+    default["tray"] = int(tray)
+    update.message.reply_text("Paper will be choosen from tray {0}.\n\nPrinting with custom configuration...".format(tray))
+    log.info(f"Paper tray will be {tray}")
+    createConfig()
+    return ConversationHandler.END
+
+@send_typing_action
+def more(update, context):
+    _more = update.message.text
+
+    if _more == "more":
+        update.message.reply_text("Enter the no of copies you want.")
+        log.info("More is choosen")
+        return COPIES
+    else:
+        update.message.reply_text("Printing with default configuration....")
+        log.info("No more choosen so default printing...")
+        createConfig()
+        return ConversationHandler.END
 
 @send_typing_action
 def color(update, context):
+    reply_keyboard = [["more", "Print"], ["/cancel"]]
     color = update.message.text
-    update.message.reply_text("Print will be in {0}\n\nPrinting.......!!!".format(color))
-    return ConversationHandler.END
+
+    default["color"] = 0 if color == "B/W" else 1
+
+    update.message.reply_text("Print will be in {0}\n\nChoose to print or want custom configurations ?".format(color),reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+    log.info(f"Choosen color is {color}")
+    return MORE
 
 def cancel(update, context):
     user = update.message.from_user
     update.message.reply_text('Bye! I hope we can talk again some day.',
                               reply_markup=ReplyKeyboardRemove())
-
+    log.warning("Cancel is pressed.")
     return ConversationHandler.END
 
 def main():
-    updater = Updater("ENTER YOUR TOCKEN", use_context=True)
+    updater = Updater("ENTER YOUR TOKEN", use_context=True)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
@@ -98,6 +151,12 @@ def main():
                      MessageHandler(Filters.document, doc)],
 
         states={
+            COLOR: [MessageHandler(Filters.text, color),
+                   CommandHandler('cancel', cancel)],
+
+            MORE: [MessageHandler(Filters.text, more),
+                   CommandHandler('cancel', cancel)],
+
             COPIES : [MessageHandler(Filters.text, copies),
                       CommandHandler('cancel', cancel)],
             
@@ -109,10 +168,7 @@ def main():
                       
             TRAY: [MessageHandler(Filters.text, tray),
                    CommandHandler('cancel', cancel)],
-                      
-            COLOR: [MessageHandler(Filters.text, color),
-                   CommandHandler('cancel', cancel)],
-
+           
         },
 
         fallbacks=[CommandHandler('cancel', cancel)]
@@ -131,5 +187,13 @@ def main():
     updater.idle()
 
 if __name__ == '__main__':
+
+    logging.basicConfig(filename=default["log_file"], level=logging.INFO, format="[%(asctime)-8s] %(levelname)-8s : %(message)s")
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    formatter = logging.Formatter("[%(asctime)-8s] %(levelname)-8s : %(message)s")
+    console.setFormatter(formatter)
+    logging.getLogger().addHandler(console)
+    global log
+    log = logging.getLogger()
     main()
-    
