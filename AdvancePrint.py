abc8da9ad1a8942ebb7e4af38e519801552939d4
Akash006 <mastermindvishu97@gmail.com>
2020-11-12 15:46:24 +0530

    Update AdvancePrint.py
    Won't ask for the tray in case of color print

--- AdvancePrint.py
+++ AdvancePrint.py
@@ -4,28 +4,31 @@ from telegram.chataction import ChatAction
 from telegram.ext import MessageHandler, Filters
 from functools import wraps
 from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
-from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
-                          ConversationHandler)
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,ConversationHandler)
 import os, time
 import datetime
 import logging
 
 COLOR, MORE, COPIES, SIZE, DUPLEX, TRAY = range(6)
 
+project_path = "/home/pi/scripts/PrintBot"
+
 default = {
     "color" : 0,
     "copies" : 1,
     "size" : "A4",
     "duplex" : 1,
-    "tray" : 1,
-    "doc_path" : "/home/pi/scripts/PrintBot/doc",
-    "photo_path" : "/home/pi/scripts/PrintBot/photo",
-    "default_file" : "/home/pi/scripts/PrintBot/config/default.cfg",
-    "log_file" : "/home/pi/scripts/PrintBot/config/printBot.log"
+    "tray" : 2,
+    "doc_path" : f"{project_path}/doc",
+    "photo_path" : f"{project_path}/photo",
+    "log_file" : f"{project_path}/config/printBot.log",
+    "default_file" : "",
 }
 
 def createConfig():
-    with open(default["default_file"], 'w') as df:
+    name = default["default_file"].split(".")[0]
+    filename = f"{project_path}/config/{name}.cfg"
+    with open(filename, 'w') as df:
         df.write("copies = " + str(default["copies"]) + "\n")
         df.write("color = " + str(default["color"]) + "\n")
         df.write("size = " + str(default["size"]) + "\n")
@@ -52,9 +55,10 @@ def photo(update, context):
     reply_keyboard = [["B/W","Color"],["/cancel"]]
     update.message.reply_text("Nice Image !!\nYou want B/W or colored print ?", reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
     newFile = update.message.photo[-1].get_file()
-    newFile.download('doc/{0}_{1}'.format(dt.strftime("%d%b%y_%H%M%S"), os.path.basename(newFile["file_path"])))
-    log.info("Photo received {0}".format(os.path.basename(newFile["file_path"])))
-    
+    newFile.download('{2}/{0}_{1}'.format(dt.strftime("%d%b%y_%H%M%S"), os.path.basename(newFile["file_path"]), default["photo_path"]))
+    default["default_file"] = os.path.basename(newFile["file_path"])
+    log.info("Photo received {0}".format(default["default_file"]))
+
     return COLOR
 
 @send_typing_action
@@ -65,8 +69,9 @@ def doc(update, context):
     updates = context.bot.get_updates()
     update.message.reply_text("Processing your document !!!\nYou want B/W or colored print ?", reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
     newFile = update.message.effective_attachment.get_file()
-    newFile.download('doc/{0}_{1}'.format(dt.strftime("%d%b%y_%H%M%S"), os.path.basename(newFile["file_path"])))
-    log.info("Document received {0}".format(os.path.basename(newFile["file_path"])))
+    newFile.download('{2}/{0}_{1}'.format(dt.strftime("%d%b%y_%H%M%S"), os.path.basename(newFile["file_path"]), default["doc_path"]))
+    default["default_file"] = os.path.basename(newFile["file_path"])
+    log.info("Document received {0}".format(default["default_file"]))
     
     return COLOR
     
@@ -90,19 +95,25 @@ def size(update, context):
     
 @send_typing_action
 def duplex(update, context):
-    reply_keyboard = [["1","2","3"], ["/cancel"]]
     duples = update.message.text
 
     default["duplex"] = 0 if duples == "Single" else 1
 
-    update.message.reply_text("OK. You want {0} side\nChoose the paper tray.".format(duples),reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+    if default["color"] == 0:
+        reply_keyboard = [["1","2","3"], ["/cancel"]]
+        update.message.reply_text("OK. You want {0} side\nChoose the paper tray.".format(duples),reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+    else:
+        update.message.reply_text("OK. You want {0} side\n\nPaper Tray is 2 for color printer".format(duples))
+        createConfig()
+        return ConversationHandler.END
+
     log.info(f"Duplex choosen is {duples}")
     return TRAY
     
 @send_typing_action
 def tray(update, context):
     tray = update.message.text
-    default["tray"] = int(tray)
+    default["tray"] = 1 if default["color"] == 1 else int(tray)
     update.message.reply_text("Paper will be choosen from tray {0}.\n\nPrinting with custom configuration...".format(tray))
     log.info(f"Paper tray will be {tray}")
     createConfig()
@@ -117,6 +128,7 @@ def more(update, context):
         log.info("More is choosen")
         return COPIES
     else:
+        default["tray"] = 1 if default["color"] == 1 else 2
         update.message.reply_text("Printing with default configuration....")
         log.info("No more choosen so default printing...")
         createConfig()
@@ -124,7 +136,7 @@ def more(update, context):
 
 @send_typing_action
 def color(update, context):
-    reply_keyboard = [["more", "Print"], ["/cancel"]]
+    reply_keyboard = [["Print", "more"], ["/cancel"]]
     color = update.message.text
 
     default["color"] = 0 if color == "B/W" else 1
@@ -159,16 +171,16 @@ def main():
 
             COPIES : [MessageHandler(Filters.text, copies),
                       CommandHandler('cancel', cancel)],
-            
+
             SIZE: [MessageHandler(Filters.text, size),
                    CommandHandler('cancel', cancel)],
-                      
+
             DUPLEX: [MessageHandler(Filters.text, duplex),
                    CommandHandler('cancel', cancel)],
-                      
+
             TRAY: [MessageHandler(Filters.text, tray),
                    CommandHandler('cancel', cancel)],
-           
+
         },
 
         fallbacks=[CommandHandler('cancel', cancel)]
